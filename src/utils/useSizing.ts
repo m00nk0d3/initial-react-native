6bda4b798b02fd3bcfb835d5d86c8c00d3e563fa
MoonKode <joaopaulomartinspessoal@gmail.com>
2022-12-17 18:21:36 -0100

    added vw to sizing hook

--- src/utils/useSizing.ts
+++ src/utils/useSizing.ts
@@ -4,7 +4,8 @@ export const useSizing = () => {
   const width = Dimensions.get('window').width;
   const height = Dimensions.get('window').height;
   const vh = (s:number) => Dimensions.get('window').height * s /100
-  return {width, height, vh}
+  const vw = (s:number) => Dimensions.get('window').width * s /100
+  return {width, height, vh, vw}
 };
 
 export default useSizing;
